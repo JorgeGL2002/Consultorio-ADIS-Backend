ae42d48d4d73e2ae1aeec8f4a4407cd8fdaf6590
Jorge Axel García Lara <167567005+JorgeGL2002@users.noreply.github.com>
2025-07-25 13:53:23 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
 
 app.use(express.json());
 app.use(cors());
