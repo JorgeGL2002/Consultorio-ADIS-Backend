e924252086cbfc5aafcbef76b1a1786b483eb4e3
Jorge Axel García Lara <167567005+JorgeGL2002@users.noreply.github.com>
2025-07-25 13:51:33 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use('/frontend', express.static(path.join(__dirname, 'frontend')));
 
 const corsOptions = {
   origin: '*',
